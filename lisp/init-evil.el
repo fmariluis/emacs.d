1f5f7de6924c99e81efe093dd6c723d2108fa470
Franco Mariluis <fmariluis@gmail.com>
2014-11-24 18:17:32 -0300

    improving cursors in evil

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -1,6 +1,25 @@
+;;; Provides evil, evil-leader, evil-matchit
 (require-package 'evil)
+(require-package 'evil-leader)
+(require-package 'evil-matchit)
+
+;;; Evil leader
+(global-evil-leader-mode)
+;;; Evil leader key works in all states
+(setq evil-leader/in-all-states 1)
 
 (require 'evil)
 (evil-mode 1)
+
+(require 'evil-matchit)
+(global-evil-matchit-mode 1)
+
+;; Cursors
+(setq evil-emacs-state-cursor '("red" box))
+(setq evil-normal-state-cursor '("green" box))
+(setq evil-visual-state-cursor '("orange" box))
+(setq evil-insert-state-cursor '("red" bar))
+(setq evil-replace-state-cursor '("red" bar))
+(setq evil-operator-state-cursor '("red" hollow))
 
 (provide 'init-evil)
